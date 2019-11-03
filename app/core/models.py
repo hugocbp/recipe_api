e2e66748ca72cc8392014c8629042f3cf63c9e1b
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-11-02 20:17:20 -0700

    Finish implementing User model and fix flake8 errors

--- app/core/models.py
+++ app/core/models.py
@@ -11,9 +11,18 @@ class UserManager(BaseUserManager):
 
         if not email:
             raise ValueError('Users must have an email address')
-        
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self.db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,7 +16,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@TEST.COM'
@@ -28,3 +27,13 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, '123123123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@test.com',
+            '123123123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
